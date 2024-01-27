9b6bf92c0b3de0fccc0a329fb9512c46a7ecc0c5
LKM0222 <python03@naver.com>
2024-01-28 08:16:35 +0900

    fix : 완드 범위공격 추가중

---
+++ Assets/Scripts/LJH/WandColliderScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using KSY;
+using LJH;
+
+public class WandColliderScript : MonoBehaviour
+{
+    //완드는 범위공격
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "enemy" || other.tag == "EnemyBoss"){
+            Enemy enemy = other.transform.GetComponent<Enemy>();
+            if(enemy.GetHP() > GameManager.Instance.dmg){ //데미지 초과의 hp가 남아있을땐
+                enemy.minusHP(GameManager.Instance.dmg); //hp 감소
+
+                HPBarScript hpbar = other.transform.GetComponent<HPBarScript>();
+                for(int i = 0;i < GameManager.Instance.dmg;i++){
+                    Destroy(hpbar.hpPop()); //객체 파괴
+                }
+            } else{ //hp가 부족하다면 
+                Destroy(other.gameObject); //enemy를 파괴
+            }
+        }
+    }
+}
