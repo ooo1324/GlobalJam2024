3c2de3637ef7d60fbda23c4bdce3db789cfa0c9b
ooo1324 <ooo1324@daum.net>
2024-01-28 11:09:37 +0900

    Add : Close bt Add, Animation Add

--- Assets/Scripts/KSY/Manager/ScoreManager.cs
+++ Assets/Scripts/KSY/Manager/ScoreManager.cs
@@ -26,34 +26,45 @@ public class ScoreManager : MonoBehaviour
     [SerializeField]
     private Image uiImage;
 
+    [SerializeField]
+    private Animator girlAnim;
+
 
     private void CheckGirlImg()
     {
         float percent = health / maxHealth * 100f;
-
+        int animSt;
         if (0 >= percent)
         {
             uiImage.sprite = girlSprites[0];
+            animSt = 0;
         }
         else if (20 >= percent)
         {
             uiImage.sprite = girlSprites[1];
+            animSt = 20;
         }
         else if (40 >= percent)
         {
             uiImage.sprite = girlSprites[2];
+            animSt = 40;
         }
         else if (60 >= percent)
         {
             uiImage.sprite = girlSprites[3];
+            animSt = 60;
         }
         else if (80 >= percent)
         {
             uiImage.sprite = girlSprites[4];
+            animSt = 80;
         }
         else
         {
             uiImage.sprite = girlSprites[5];
+            animSt = 100;
         }
+
+        girlAnim.Play($"{animSt}%Face");
     }
 }
